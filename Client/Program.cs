8de53b8a0a14bf13ce11cf53b3fb08accc975af1
eirikkar <eirikkar@proton.me>
2025-01-07 13:55:31 +0100

    add client class and start client to main

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ChatClient client = new ChatClient("127.0.0.1", 4444);
+        client.StartClient();
     }
 }
